73fb8b01e633aad8f623d4a11e47ee1562dd081e
wedoqa <jelena@wedoqa.com>
2023-01-24 08:57:10 +0100

    Homework - My info branch

--- cypress/pageObjects/basePage.js
+++ cypress/pageObjects/basePage.js
@@ -1,10 +1,15 @@
 export class BasePage {
     constructor() {
         this.adminLink = '[href="/web/index.php/admin/viewAdminModule"]'
+        this.myInfoLink = '[href="/web/index.php/pim/viewMyDetails"]'
     }
 
     openAdminPage () {
         cy.get(this.adminLink).click();
+    }
+
+    openMyInfoPage () {
+        cy.get(this.myInfoLink).click()
     }
 }
 
---
+++ cypress/pageObjects/myInfoPage.js
@@ -0,0 +1,90 @@
+export class MyInfoPage {
+    constructor(a) {
+        this.firstNameInput = '[class="oxd-input oxd-input--active orangehrm-firstname"]';
+        this.middleNameInput = '[class="oxd-input oxd-input--active orangehrm-middlename"]';
+        this.lastNameInput = '[class="oxd-input oxd-input--active orangehrm-lastname"]';
+        this.nickNameInput = '[class="oxd-input oxd-input--active"]';
+        this.employeeId = '.orangehrm-edit-employee-content > .orangehrm-horizontal-padding > .oxd-form > div:nth-of-type(2) > *:first-child > *:first-child > div > :nth-of-type(2) > input';
+        this.otherId = '.orangehrm-edit-employee-content > .orangehrm-horizontal-padding > .oxd-form > div:nth-of-type(2) > *:first-child > :nth-of-type(2) > .oxd-input-group > :nth-of-type(2) > input';
+        this.driversLicenseNumber = '[class="oxd-input oxd-input--active"]';
+        this.licenseExpiryDate = '.oxd-date-input';
+        this.firstSaveButton = '.oxd-form-actions > *:nth-child(2)';
+        this.secondSaveButton = '.oxd-form-actions > *:only-child';
+        this.succesModal = '.oxd-toast';
+        this.customFields = '.orangehrm-custom-fields > .orangehrm-card-container > .oxd-form  > .oxd-form-row > .oxd-grid-3'
+        this.nationality = '.oxd-select-text.oxd-select-text--active';
+        this.menu = '[role="listbox"]';
+        this.maritalStatus = '.oxd-select-text.oxd-select-text--active';
+        this.bloodtypeDropdown = '[role="listbox"]'
+    }
+
+    enterFirstName(firstname) {
+        cy.get(this.firstNameInput).clear().type(firstname);
+    }
+
+    enterMiddleName(middlename) {
+        cy.get(this.middleNameInput).clear().type(middlename);
+    }
+
+    enterLastName(lastname) {
+        cy.get(this.lastNameInput).clear().type(lastname);
+    }
+
+    enterNickName(nickname) {
+        cy.get(this.nickNameInput).eq(1).clear().type(nickname);
+    }
+
+    enterEmployeeId(employeeid) {
+        cy.get(this.employeeId).should('be.visible')
+        cy.get(this.employeeId).clear().type(employeeid);
+    }
+
+    enterOtherId(otherid) {
+        cy.get(this.otherId).type(otherid);
+    }
+
+    enterDriversLicenseNumber(driverslicensenumber){
+        cy.get(this.driversLicenseNumber).eq(2).clear().type(driverslicensenumber);
+    }
+
+    selectLicenseExpiryDate(date) {
+        cy.get(this.licenseExpiryDate).eq(0).click();
+        cy.contains(date).click();
+    }
+
+    clickOnFirstSaveButton() {
+        cy.get(this.firstSaveButton).click();
+    }
+
+    clickOnSecondSaveButton(){
+        cy.get(this.secondSaveButton).click();
+    }
+
+    verifySuccesModal(modaltext){
+        cy.get(this.succesModal).should('include.text', modaltext)
+    }
+
+    checkRadioBtn(value) {
+        cy.get(`input[value=${value}] + *`).click()
+    }
+
+    clickBloodtypeDropdown () {
+        cy.get(this.customFields).find('> *:first-child').click()
+    }
+
+    selectFromBloodTypeDropdown(bloodtype) {
+        cy.get(this.bloodtypeDropdown).find('span').contains(bloodtype).click()
+    }
+
+    selectNationality(nationality){
+        cy.get(this.nationality).eq(0).click();
+        cy.get(this.menu).contains(nationality).click();
+    }
+
+    selectMaritalStatus(maritalstatus){
+        cy.get(this.maritalStatus).eq(1).click();
+        cy.get(this.menu).contains(maritalstatus).click();
+    }s
+}
+
+export const myInfoPage = new MyInfoPage();
