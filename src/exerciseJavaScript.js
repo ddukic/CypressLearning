cb7bde2403924d74c27cc899460bf4b53d0e5dc2
Branko <brankopauk@gmail.com>
2023-02-01 14:45:00 +0100

    exercise2

---
+++ src/exerciseJavaScript.js
@@ -0,0 +1,104 @@
+// 1. Napisati program koji ispisuje sledeći string:
+// "The memory of that scene for me is like a frame of film forever frozen at that moment: the red carpet, the green lawn, the white house, the leaden sky. The new president and his first lady. - Richard M. Nixon"
+// Reči 'red', 'green' i 'white' u datom stringu iskoristiti iz sledećeg niza:
+// var colors = ['white', 'green', 'red', 'blue', 'black'];
+
+// 2. Napisati program koji kreira niz: colors = ['white', 'green', 'red', 'blue', 'black'];
+// i ispisuje njegove elemente. Koristiti for-of petlju
+
+// 3. Kreirati niz imena meseca u godini i pomoću for-of petlje ispisati sve elemente niza
+
+// 4. Napraviti dva niza, a i b, sa proizvoljnim brojem elemenata. Ako je broj elementa u nizu a veći od broja elemeneta u nizu b, ispisati sve elemenete niza a. U suprotnom, ispisati elemente niza b
+
+// 5. Ispisati elemente proizvoljnog niza brojeva pomoću for petlje
+
+// 6. Ispisati elemente proizvoljnog niza brojeva, uvećane za 1 (nije potrebno menjati sadržaj samog niza)
+
+// 7. Ispisati elemente proizvoljnog niza brojeva, duplirane (nije potrebno menjati sadržaj samog niza)
+
+// 8. Ispisati elemente proizvoljnog niza brojeva, unazad
+
+// 9. Ispisati svaki drugi element proizvoljnog niza
+
+// 10. Sabrati sve elemente proizvoljnog niza brojeva i ispisati zbir na kraju
+
+// 11. Napisati program koji računa srednju vrednost elemenata proizvoljnog niza brojeva
+
+// 12. Napisati program koji za dati niz objekata:
+// var capitals = [
+// { country: 'Germany', city: 'Berlin' },
+// { country: 'China', city: 'Beijing' },
+// { country: 'Portugal', city: 'Lisabon' },
+// { country: 'Spain', city: 'Madrid' },
+// { country: 'Japan', city: 'Tokyo' },
+// ];
+// ispisuje sledeće:
+//         The capital of Germany is Berlin
+//         The capital of China is Beijing 
+//         The capital of Portugal is Lisabon
+//         The capital of SPain is Madrid
+//         The capital of Japan is Tokyo
+
+// 13. Napraviti prazan niz u promenljivoj prirodniBrojevi. U ovaj niz ubaciti prvih 100 prirodnih brojeva pomoću for petlje. Ispisati u konzoli sadržaj niza na kraju
+
+// 14. Napraviti dva prazna niza, parni i neparni. Koristeći jednu for petlju, postaviti sve parne brojeve od 1 do 100 u niz parni, i sve neparne brojeve od 1 do 100 u niz neparni. Ispisati sadržaj nizova
+
+// 15. Napisati program koji pravi i ispisuje niz svih brojeva u intervalu od broja a do broja b
+
+// 16. Napisati program koji za dati niz arr i broj num proverava da li se taj broj nalazi u nizu. Ispisati 'Broj se nalazi u nizu' ako je pronađen, ili 'Broj se ne nalazi u nizu' ako nije
+
+// 17. Napisati program koji nalazi i ispisuje najveći element proizvoljnog niza brojeva
+
+// 18. Napraviti dva niza, a i b, sa jednakim brojem elemenata tipa number. Proći kroz oba niza paralelno for petljom i ispisivati zbir elemenata nizova a i b. Dakle, ispisati zbir prvog elementa niza a sa prvim elementom niza b, zbir drugog a sa drugim iz b itd.
+
+// 19. Napraviti dva niza, a i b, koji sadrže po 10 stringova. Pomoću for petlje napraviti objekat c koji koristi elemente niza a kao ključeve, i elemente niza b kao vrednosti. Ispisati objekat c u konzoli
+
+// 20. Napraviti promenljivu meseci i dodeliti joj vrednost niza objekata koji kao property-e sadrži ime meseca i broj dana u tom mesecu ([ { mesec: 'Januar', brojDana: 31 }, { mesec: 'Februar', brojDana: 28 }, ... ]). Prolazeći kroz ovaj niz, ispisati sve moguće datume u godini (dakle '1. Januar', '2. Januar'… '31. Januar', '1. Februar', '2. Februar'…)
+
+// 21. Ispisati brojeve od 0 do 5 koristeći for petlju
+
+// 22. Ispisati brojeve od 20 do 30 koristeći while petlju
+
+// 23. Ispisati parne brojeve od 0 do 10 koristeći for petlju
+
+// 24. Ispisati prvih 10 neparnih prirodnih brojeva koristeći while petlju
+
+// 25. Ispisati prvih 10 neparnih prirodnih brojeva koristeći for petlju
+
+// 26. Ispisati sve brojeve od 10 do 0 (po opadajućem redosledu)
+
+// 27. Ispisati sve brojeve u datom opsegu (definisati dve promenljive koje sadrze pocetak i kraj, i ispisati sve brojeve izmedju njih)
+
+// 28. Napisati program koji sabira prvih 10 prirodnih brojeva kroz petlju. Ispisati rezultat
+
+// 30. Napisati program koji sabira prvih 20 neparnih prirodnih brojeva. Ispisati rezultat
+
+// 31. Napisati program koji vrši množenje dva broja bez korišćenja operatora *, tako što sabira broj sa samim sobom više puta (npr. 4 * 5 se računa kao 5 + 5 + 5 + 5)
+
+// 32. Napisati program koji vrši stepenovanje dva broja, tako što množi broj sa samim sobom više puta (npr. 2⁵ se računa kao 2 * 2 * 2 * 2 * 2)
+
+// 33. Napisati program koji za dati broj ispisuje proizvode svih brojeva od 1 do 10 sa tim brojem (npr. za zadati broj 7 ispisati 7, 14, 21, 28, 35, 42, 49, 56, 63, 70)
+
+// 34. Napisati program koji za dati broj ispisuje proizvode svih brojeva od 1 do 10 sa tim brojem, odvojene razmacima (npr. za zadati broj 7 ispisati string '7 14 21 28 35 42 49 56 63 70'). Spajanje stringova se može izvršiti pomoću operatora + ili +=, npr:
+// var str = 'Hello'; str += ' world';
+
+// 35. Napisati program koji računa faktorijel datog broja. Faktorijel nekog broja je proizvod svih brojeva od 1 do tog broja (npr. faktorijel broja 5 je 1 * 2 * 3 * 4 * 5 = 120)
+
+// 36. Napisati program koji ispisuje brojeve od 1 do 100. Međutim, ako je broj deljiv sa 3 ispisati "Fizz" umesto tog broja, ako je deljiv sa 5 ispisati "Buzz" umesto broja, a ako je deljiv i sa 3 i sa 5 ispisati "FizzBuzz"
+
+// 37. Napisati program koji kroz petlju broji, i na kraju ispisuje, koliko brojeva od 1 do 100 je deljivo sa 3 (rezultat treba da bude 33)
+
+// 38. Napisati program koji kroz petlju broji, i na kraju ispisuje, koliko brojeva u datom opsegu (definisati dve promenljive koje sadrze pocetak i kraj) je deljivo sa 3
+
+// 39. Napisati program koji kroz petlju broji, i na kraju ispisuje, koliko brojeva u datom opsegu je deljivo sa 3, a koliko sa 7
+
+// 40. Ispisati prvih 10 stepeni dvojke (2, 4, 8, 16… 1024)
+
+// 41. Napisati program koji računa i ispisuje prvih 20 brojeva u Fibonačijevom nizu. Fibonačijev niz počinje od 1 i svaki sledeći broj je zbir prethodna dva. Npr. prvih deset brojeva su:
+// 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+
+// 42. Ispisati sve proste brojeve od 1 do 100
+
+// 43. Napisati prvih 27 brojeva deljivih i sa 7 i sa 13.
+
+// 44. Bacati kockicu (sa 6 strana) dok ne dobijemo broj 6. Ispisati koliko puta smo bacili kocku dok nismo dobili broj 6. Koristiti Math.random() za bacanje kockice. https://www.w3schools.com/js/js_random.asp
